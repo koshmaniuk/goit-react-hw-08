ce08b2ecc02197cbb3031eb9e52d6665291aaad0
koshmaniuk <trident93@gmail.com>
2024-03-15 03:38:38 -0400

    import fix

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,27 +1,12 @@
-import DocumentTitle from "../../components/DocumentTitle";
+import DocumentTitle from "../../components/DocumentTitle/DocumentTitle";
 import css from "./Home.module.css";
-
-const styles = {
-  container: {
-    minHeight: "calc(100vh - 50px)",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontWeight: 500,
-    fontSize: 48,
-    textAlign: "center",
-  },
-};
 
 export default function Home() {
   return (
     <>
       <DocumentTitle>Home</DocumentTitle>
-
-      <div style={styles.container}>
-        <h1 style={styles.title}>
+      <div style={css.container}>
+        <h1 style={css.title}>
           Phonebook by Yaroslav Koshmaniuk{" "}
           <span role="img" aria-label="Greeting icon">
             💁‍♀️
